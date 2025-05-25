0561efbf0d3db8c44657cc052d6122844440580d
Aymeric Pellé <aympelle@gmail.com>
2025-05-25 18:19:45 +0200

    Implement neighbor positions functions.

--- neighborhood.py
+++ neighborhood.py
@@ -2,25 +2,55 @@ from grid import Grid
 from vec2 import Vec2
 
 
+def ortho_positions(pos: Vec2, distance=1):
+    positions = []
+    for j in range(distance + 1):
+        ori_pos = pos - Vec2(distance - j, j)
+        for i in range((distance - j) * 2 + 1):
+            npos = ori_pos + Vec2(i, 0)
+            positions.append(npos)
+    for j in range(1, distance + 1):
+        ori_pos = pos + Vec2(-(distance - j), j)
+        for i in range((distance - j) * 2 + 1):
+            npos = ori_pos + Vec2(i, 0)
+            positions.append(npos)
+    return positions
+
+
+def square_positions(pos: Vec2, distance=1):
+    positions = []
+    origin = pos - Vec2(distance, distance)
+    square_size = distance * 2 + 1
+    for j in range(square_size):
+        for i in range(square_size):
+            npos = origin + Vec2(i, j)
+            positions.append(npos)
+    return positions
+
+
 def ortho_neighbor_positions(pos: Vec2, distance=1):
-    return []
+    positions = ortho_positions(pos, distance)
+    positions.remove(pos)
+    return positions
 
 
 def square_neighbor_positions(pos: Vec2, distance=1):
-    return []
+    positions = square_positions(pos, distance)
+    positions.remove(pos)
+    return positions
 
 
 def ortho_neighbor_positions_in_grid(grid: Grid, pos: Vec2, distance = 1):
-    return []
+    return filter(lambda p: grid.contains(p), ortho_neighbor_positions(pos, distance))
 
 
 def square_neighbor_positions_in_grid(grid: Grid, pos: Vec2, distance = 1):
-    return []
+    return filter(lambda p: grid.contains(p), square_neighbor_positions(pos, distance))
 
 
 def ortho_positions_in_grid(grid: Grid, pos: Vec2, distance = 1):
-    return []
+    return filter(lambda p: grid.contains(p), ortho_positions(pos, distance))
 
 
 def square_positions_in_grid(grid: Grid, pos: Vec2, distance = 1):
-    return []
+    return filter(lambda p: grid.contains(p), square_positions(pos, distance))
--- tests/test_neighbourhood.py
+++ tests/test_neighbourhood.py
@@ -1,7 +1,3 @@
-import filecmp
-import os
-import shutil
-from pathlib import Path
 from unittest import TestCase
 
 from neighborhood import square_neighbor_positions, ortho_neighbor_positions
@@ -12,23 +8,36 @@ class TestNeighborhood(TestCase):
     def test__square_neighbor_positions__0_0_d1__expected_list(self):
         distance = 1
         positions = square_neighbor_positions(Vec2(0, 0), distance)
-        expected_positions = []
+        expected_positions = [Vec2(-1,-1), Vec2(0,-1), Vec2(1,-1),
+                              Vec2(-1,0), Vec2(1,0),
+                              Vec2(-1,1), Vec2(0,1), Vec2(1,1)
+                              ]
         self.assertEqual(positions, expected_positions)
 
     def test__ortho_neighbor_positions__0_0_d1__expected_list(self):
         distance = 1
         positions = ortho_neighbor_positions(Vec2(0, 0), distance)
-        expected_positions = []
+        expected_positions = [Vec2(-1,0), Vec2(1,0), Vec2(0,-1), Vec2(0,1)]
         self.assertEqual(positions, expected_positions)
 
     def test__square_neighbor_positions__0_0_d2__expected_list(self):
         distance = 2
         positions = square_neighbor_positions(Vec2(0, 0), distance)
-        expected_positions = []
+        expected_positions = [Vec2(-2,-2), Vec2(-1,-2), Vec2(0,-2), Vec2(1,-2), Vec2(2,-2),
+                              Vec2(-2,-1), Vec2(-1,-1), Vec2(0,-1), Vec2(1,-1), Vec2(2,-1),
+                              Vec2(-2,0), Vec2(-1,0), Vec2(1,0), Vec2(2,0),
+                              Vec2(-2,1), Vec2(-1,1), Vec2(0,1), Vec2(1,1), Vec2(2,1),
+                              Vec2(-2,2), Vec2(-1,2), Vec2(0,2), Vec2(1,2), Vec2(2,2),
+                              ]
         self.assertEqual(positions, expected_positions)
 
     def test__ortho_neighbor_positions__0_0_d2__expected_list(self):
         distance = 2
         positions = ortho_neighbor_positions(Vec2(0, 0), distance)
-        expected_positions = []
+        expected_positions = [Vec2(-2,0), Vec2(-1,0), Vec2(1,0), Vec2(2,0),
+                              Vec2(-1,-1), Vec2(0,-1), Vec2(1,-1),
+                              Vec2(0,-2),
+                              Vec2(-1,1), Vec2(0,1), Vec2(1,1),
+                              Vec2(0,2)
+                              ]
         self.assertEqual(positions, expected_positions)
