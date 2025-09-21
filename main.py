225093224e7c17a6511e73946101f15d879291a0
Aymeric Pellé <aympelle@gmail.com>
2025-09-21 14:56:37 +0200

    Add some functions to edit and print a terrain.

--- main.py
+++ main.py
@@ -20,15 +20,28 @@ def random_positions(grid: Grid, n: int):
         square_positions.append(sq_pos)
     return square_positions
 
+
+def print_terrain(grid: Grid):
+    for j in range(grid.height):
+        for i in range(grid.width):
+            print(f"[{grid[Vec2(i, j)]}]", end="")
+        if j < (grid.dim.y - 1):
+            print()
+
+
+def set_root_obstacles(grid: Grid, percent: int = 12):
+    nb_root_obstacles = (percent * grid.width * grid.height) // 100
+    positions = random_positions(grid, nb_root_obstacles)
+    # print(f"{positions}")
+    for pos in positions:
+        grid[pos] = "#"
+
+
 if __name__ == "__main__":
     rng_seed = random.randint(0, 100)
     print(f"seed: {rng_seed}")
     random.seed(rng_seed)
     #-----
-    grid = Grid(Vec2(10, 10), "[ ]")
-    nb_root_obstacles = 12
-    positions = random_positions(grid, nb_root_obstacles)
-    print(f"{positions}")
-    for pos in positions:
-        grid[pos] = "[#]"
-    print(f"{grid}")
+    grid = Grid(Vec2(10, 10), " ")
+    set_root_obstacles(grid)
+    print_terrain(grid)
